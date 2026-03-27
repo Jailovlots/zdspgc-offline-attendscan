edd5872e23094dde923de2dcd010a0497562ab73
jailovlots <hajejahusin28@gmail.com>
2026-03-27 10:28:31 +0800

    third

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
+
+// API Root Status Route (Requested)
+// Note: This overrides serving the React frontend on the exact '/' path. 
+// If you want the React frontend to load on '/', you should remove this or change to '/api/status'
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 // Serve static frontend files with CDN/Browser caching enabled
 app.use(express.static(path.join(__dirname, '../dist'), {
   setHeaders: (res, filePath) => {
